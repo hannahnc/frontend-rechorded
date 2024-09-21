96814bc58550badf91b14ae0dd0024ac0bc314c3
Hannah <104505857+hannahnc@users.noreply.github.com>
2024-09-21 09:51:17 +0800

    Update DownloadPage.jsx

--- src/pages/DownloadPage.jsx
+++ src/pages/DownloadPage.jsx
@@ -1,2 +1,14 @@
-// import { Box, Heading, Image, Text } from "@chakra-ui/react";
-// import { Navbar } from "../components/Homepage/Navbar";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { PDFViewer } from '@react-pdf/renderer';
+import MyDocument from './MyDocument';
+
+const App = () => (
+  <PDFViewer width="1500vw" height="800vh" showToolbar>
+    <MyDocument />
+  </PDFViewer>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App;
